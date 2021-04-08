26404bb7bf4f8216ad6ef52e955ea69b634cb5c5
Daniele Thomas <danieleSThomas6@gmail.com>
2021-04-08 19:27:09 -0400

    Adding Data Layer

---
+++ SharkTracker.Data/Ping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTracker.Data
+{
+   public class Ping
+    {
+        [Required]
+        public int PingId { get; set; }
+
+        [Required]
+        public string PingLocation { get; set; }
+
+        [ForeignKey(nameof(tag)),Required]
+        public int TagNumber { get; set; }
+        public virtual Tag tag { get; set; }
+
+
+        [ForeignKey(nameof(shark)),Required]
+        public int SharkId { get; set; }
+        public virtual Shark shark { get; set; }
+
+
+        [Required]
+        public DateTime dateTime { get; set; }
+    }
+}
---
+++ SharkTracker.Data/Shark.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTracker.Data
+{
+    public class Shark
+    {
+       [Required]
+        public int SharkId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public Enum Species { get; set; }
+
+        [Required] 
+        public int Length { get; set; }
+
+        [Required]
+        public Enum Sex { get; set; }
+
+        [Required]
+        public int Weight { get; set; }
+
+        [Required]
+        public string SharkName { get; set; }
+
+        [Required]
+        public Enum Age { get; set; }
+
+    }
+}
