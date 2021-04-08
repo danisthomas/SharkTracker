e7e4e52e5d6fa408a5d51f5438d9a2e0c68da6e6
Daniele Thomas <danieleSThomas6@gmail.com>
2021-04-08 18:55:25 -0400

    Add project files.

---
+++ SharkTracker.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharkTracker.WebMVC.MvcApplication" Language="C#" %>
---
+++ SharkTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharkTracker.WebMVC.Startup))]
+namespace SharkTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
