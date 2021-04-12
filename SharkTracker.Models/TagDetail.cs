92fe82420a1c028296877a084b69e9bb4d911d0c
Daniele Thomas <danieleSThomas6@gmail.com>
2021-04-12 10:25:36 -0400

    Adding Tag Models

---
+++ SharkTracker.Models/TagDetail.cs
@@ -0,0 +1,26 @@
+﻿using SharkTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTracker.Models
+{
+    public class TagDetail
+    {
+        public int TagNumber { get; set; }
+
+        [Required]
+        public string TagLocation { get; set; }
+
+        [Required]
+        public DateTime TagDate { get; set; }
+
+        [ForeignKey(nameof(shark)), Required]
+        public int SharkId { get; set; }
+        public virtual Shark shark { get; set; }
+    }
+}
